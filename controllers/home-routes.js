caea5fcf431b62d19571df812a50d7b26f33c2ac
kristynerhaugen <kristynerhaugen@gmail.com>
2022-07-26 12:30:58 -0500

    authentication working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,8 @@ const { Post, User, Comment } = require('../models');
 
 // homepage route
 router.get('/', (req, res) => {
+    console.log(req.session);
+
     Post.findAll({
         attributes: [
             'id',
@@ -38,4 +40,15 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.get('/login', (req, res) => {
+
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers/');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
@@ -18,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
-app.use(routes);
+app.use(require('./controllers/'));
 
 // turn on connection to database and server
 sequelize.sync({ force: true }).then(() => {
