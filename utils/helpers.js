6c9249439842940c5b040646a6376e4a537e4a0b
kristynerhaugen <kristynerhaugen@gmail.com>
2022-07-29 10:04:29 -0500

    application complete

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,13 +4,6 @@ module.exports = {
       date
     ).getDate()}/${new Date(date).getFullYear()}`;
   },
-  format_plural: (word, amount) => {
-    if (amount !== 1) {
-      return `${word}s`;
-    }
-
-    return word;
-  },
 
   format_url: (url) => {
     return url
@@ -19,5 +12,13 @@ module.exports = {
       .replace("www.", "")
       .split("/")[0]
       .split("?")[0];
+  },
+
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+
+    return word;
   },
 };
